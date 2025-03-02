4b8becf54741ed6931729f5ba768e09ac3dd4697
Steven <1347428036@qq.com>
2025-03-02 22:32:19 +0800

    Initial commit

---
+++ src/test/java/com/esmiao/cloudpicture/CloudPictureDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmiao.cloudpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPictureDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
